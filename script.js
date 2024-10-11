ba2fce4e4f198ccc39c29481233e531f7b7da3b5
Kaicheng1014 <135619585+Kaicheng1014@users.noreply.github.com>
2024-10-11 09:45:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     if (q2Answer && q2Answer.value === 'A') {
         score++;
     }
+    
+    // 檢查第三題的答案
+    const q2Answer = document.querySelector('input[name="q2"]:checked');
+    if (q2Answer && q2Answer.value === 'B') {
+        score++;
+    }
 
+   // 檢查第四題的答案
+    const q2Answer = document.querySelector('input[name="q2"]:checked');
+    if (q2Answer && q2Answer.value === 'B') {
+        score++;
+    }
+
+   // 檢查第五題的答案
+    const q2Answer = document.querySelector('input[name="q2"]:checked');
+    if (q2Answer && q2Answer.value === 'A') {
+        score++;
+    }
     // 顯示結果
     const resultText = `你的分數是：${score} / ${totalQuestions}`;
     document.getElementById('result').innerText = resultText;
